476c5e4e384420ff848466440ef0cbea109c44c0
Agata Sumowska <agata.sumowska@gmail.com>
2016-10-24 11:33:22 +0100

    Adding a list of students

---
+++ directory.rb
@@ -0,0 +1,18 @@
+puts "The students of Villains Academy"
+puts "--------------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Micheal Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "Joker"
+puts "Joeffrey Baratheon"
+puts "Norman Bates"
+puts "Winston Churchill"
+
+print "Overall, we have "
+print 11
+puts " great students"
