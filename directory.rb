bee40f427c6d0a583a7e0a8c911fe1f93bbcde4c
Agata Sumowska <agata.sumowska@gmail.com>
2016-10-30 04:12:40 +0000

    Creating a gitignore file and adding the option to save names of students in a separate file

--- directory.rb
+++ directory.rb
@@ -12,10 +12,11 @@ $cohorts = ["January",
           "December"
 ]
 
+@students = []
+
  def input_students
    puts "Enter the names of the students"
    puts "To finish, just hit return twice"
-   students = []
    name = gets.delete("\n")
    while !name.empty? do
      cohort = ""
@@ -26,14 +27,14 @@ $cohorts = ["January",
      	    cohort = "November"
         end
      end
-     students << {name:name, cohort: cohort.to_sym}
-     puts "Now we have #{students.count} students"
+     @students << {name:name, cohort: cohort.to_sym}
+     puts "Now we have #{@students.count} students"
      name = gets.chomp
    end
-   if students == []
+   if @students == []
      exit
    end
-   students
+   @students
  end
 
 
@@ -43,57 +44,71 @@ $cohorts = ["January",
    puts "--------------------".center(line_width)
  end
 
- def prints(names)
-  names =	names.sort_by { |entry| entry[:cohort]}
+ def print_students_list
+  @students =	@students.sort_by { |entry| entry[:cohort]}
  	index = 0
-   while index < names.length do
-     puts "#{index + 1}. #{names[index][:name]} (#{names[index][:cohort]} cohort)"
+   while index < @students.length do
+     puts "#{index + 1}. #{@students[index][:name]} (#{@students[index][:cohort]} cohort)"
      index += 1
     end
-   end
+ end
 
 
-def students_starting_with(students)
-  puts "Print students' names starting with letter ..."
-  char = gets.chomp
-  puts "and shorter then 12 characters"
-  students.each_with_index {|student, index|
-    if student[:name][0].downcase == char && student[:name].length < 12
-      then puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  }
+def print_footer
+  line_width = 80
+  if @students.length == 1
+    print "Overall, we have 1 great student ".center(line_width)
+  else
+    print "Overall, we have #{@students.count} great students ".center(line_width)
+  end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(", ")
+    file.puts csv_line
+  end
+  file.close
 end
 
 
-def print_footer(names)
-  line_width = 80
-  if names.length == 1
-    print "Overall, we have 1 great student ".center(line_width)
+def print_menu
+  puts "1. input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process (selection)
+  case selection
+  when "1"
+    @students = input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
   else
-    print "Overall, we have #{names.count} great students ".center(line_width)
+    puts "I don't know what you mean, try again"
   end
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      prints(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
+
+
 
 interactive_menu
